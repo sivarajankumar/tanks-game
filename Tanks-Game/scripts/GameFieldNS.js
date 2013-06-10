1acf6ca3928ddaa4c2e7258a6ea98140ecbdfa8e
Vladislav.Karamfilov <Vladislav.Karamfilov@gmail.com>
2013-06-10 16:11:56 +0000

    Content of FieldPosition.js added to GameFieldNS.js

--- Tanks-Game/scripts/GameFieldNS.js
+++ Tanks-Game/scripts/GameFieldNS.js
@@ -7,7 +7,22 @@
         }
     });
 
+    var FieldPosition = Class.create({
+        initialize: function (xCoord, yCoord) {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+        },
+        update: function (delta) {
+            this.xCoord += delta.xCoord;
+            this.yCoord += delta.yCoord;
+        },
+        equals: function (other) {
+            return this.xCoord == other.xCoord && this.yCoord == other.yCoord;
+        }
+    });
+
     return {
-        Field: Field
+        Field: Field,
+        FieldPosition: FieldPosition
     }
 })();
