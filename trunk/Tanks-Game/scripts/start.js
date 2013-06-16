1ed870e7a163d3af5e5c3ca0f00618261c290591
sdmsdm12@gmail.com <sdmsdm12@gmail.com@4edfae09-b167-37bd-9ca7-946783332495>
2013-06-16 13:41:08 +0000

    start screen update

--- trunk/Tanks-Game/scripts/start.js
+++ trunk/Tanks-Game/scripts/start.js
@@ -1,16 +1,9 @@
-﻿$(document).ready(function () {
-    var delay = 5000;
-
-    var startFrame = $('<div id="wrapper"></div>');
-    $('<p id="inner-text">TANKS GAME</br>created by</br> DRAGON FRUIT</p>').appendTo(startFrame);
-    startFrame.appendTo(document.body).fadeOut(delay - 35);
-
-    var interval = setInterval(function () {
-        clearInterval(interval); //style="opacity: 0"
-        var btn = $('<button></button>');
-        var img = $('<img src="images/start.png"/>');
-        img.fadeIn(delay / 3);
-        img.appendTo(btn);
-        btn.appendTo(document.body);
-    }, delay);
-});
+﻿var delay = 3000;
+//$("#myGame").remove();
+var startFrame = $('<div id="wrapper"></div>');
+$('<p id="inner-text">TANKS GAME</br>created by</br> DRAGON FRUIT</p>').appendTo(startFrame);
+startFrame.appendTo(document.body).fadeOut(delay - 3);
+var interval = setInterval(function () {
+    clearInterval(interval);
+    var btn = $('<a href="#" id="startButton"></a>').fadeIn(delay / 3).appendTo(document.body);
+}, delay + 3);
