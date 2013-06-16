a510ad18fb86250f7080270b7771a70bb4169c32
Vladislav.Karamfilov@gmail.com <Vladislav.Karamfilov@gmail.com@4edfae09-b167-37bd-9ca7-946783332495>
2013-06-16 08:55:15 +0000

    Options added to game objects

--- trunk/Tanks-Game/scripts/GameObjectsNS.js
+++ trunk/Tanks-Game/scripts/GameObjectsNS.js
@@ -1,9 +1,10 @@
 var gameObjectsNS = (function () {
     var GameObject = Class.create({
-        initialize: function (id, animation, topLeft) {
+        initialize: function (id, animation, topLeft, options) {
             this.id = id;
             this.animation = animation;
             this.topLeft = topLeft;
+            this.options = options;
         },
         getRendered: function () {
             var container;
@@ -18,14 +19,14 @@ var gameObjectsNS = (function () {
     });
 
     var StaticObject = Class.create(GameObject, {
-        initialize: function ($super, id, animation, topLeft) {
-            $super(id, animation, topLeft);
+        initialize: function ($super, id, animation, topLeft, options) {
+            $super(id, animation, topLeft, options);
         }
     });
 
     var MovingObject = Class.create(GameObject, {
-        initialize: function ($super, id, animation, topLeft, speed) {
-            $super(id, animation, topLeft);
+        initialize: function ($super, id, animation, topLeft, speed, options) {
+            $super(id, animation, topLeft, options);
             this.speed = speed;
         },
         move: function () {
@@ -35,26 +36,25 @@ var gameObjectsNS = (function () {
 
     // Static objects
     var BrickWall = Class.create(StaticObject, {
-        initialize: function ($super, id, animation, topLeft) {
-            $super(id, animation, topLeft);
+        initialize: function ($super, id, animation, topLeft, options) {
+            $super(id, animation, topLeft, options);
             this.isDestroyed = false;
         }
     });
 
     var SteelWall = Class.create(StaticObject, {
-        initialize: function ($super, id, animation, topLeft) {
-            $super(id, animation, topLeft);
+        initialize: function ($super, id, animation, topLeft, options) {
+            $super(id, animation, topLeft, options);
         }
     });
 
     // Moving objects
     var PlayerTank = Class.create(MovingObject, {
-        initialize: function ($super, id, animation, topLeft, speed) {
-            $super(id, animation, topLeft, speed);
+        initialize: function ($super, id, animation, topLeft, speed, options) {
+            $super(id, animation, topLeft, speed, options);
         },
         move: function (direction) {
             this.topLeft.update(direction);
-            this.lastMoveDirection = direction;
         },
         shoot: function () {
           // TODO: Implement logic to shoot!!!  // this.shouldShoot = true;
@@ -62,8 +62,8 @@ var gameObjectsNS = (function () {
     });
 
     var EnemyTank = Class.create(MovingObject, {
-        initialize: function ($super, id, animation, topLeft, speed) {
-            $super(id, animation, topLeft, speed);
+        initialize: function ($super, id, animation, topLeft, speed, options) {
+            $super(id, animation, topLeft, speed, options);
             this.isAlive = true;
         },
         move: function () {
@@ -72,14 +72,14 @@ var gameObjectsNS = (function () {
     });
 
     var Bullet = Class.create(MovingObject, {
-        initialize: function ($super, id, animation, topLeft, speed, direction) {
-            $super(id, animation, topLeft, speed);
+        initialize: function ($super, id, animation, topLeft, speed, direction, options) {
+            $super(id, animation, topLeft, speed, options);
             this.isDestroyed = false;
             this.direction = direction;
             // TODO: Property for shot origin!!!
         },
         move: function () {
-            this.topLeft.update(this.Direction); // TODO: Reconsider!!!
+            this.topLeft.update(this.direction); // TODO: Reconsider!!!
         }
     });
 
