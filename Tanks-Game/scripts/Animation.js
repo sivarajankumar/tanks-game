2852c74c3aac1a9a2fb8764c5b17f14b8b62abab
Vladislav.Karamfilov <Vladislav.Karamfilov@gmail.com>
2013-06-16 08:55:15 +0000

    Options added to game objects

--- Tanks-Game/scripts/Animation.js
+++ Tanks-Game/scripts/Animation.js
@@ -1,9 +1,7 @@
 ﻿var Animation = Class.create({
-    initialize: function (url, width, hFlip, vFlip, numberOfFrames, currentFrame, rate) {
+    initialize: function (url, width, numberOfFrames, currentFrame, rate) {
         this.url = url;
         this.width = width;
-        this.horizontalFlip = hFlip;
-        this.verticalFlip = vFlip;
         this.numberOfFrames = numberOfFrames;
         this.currentFrame = currentFrame;
         this.rate = rate;
